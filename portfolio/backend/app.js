526d155308c395cb3aa722539acbe630030e2282
Mathias Salva <mattsalva@outlook.com>
2023-04-09 04:47:27 -0300

    Agregando log in y logout con ruta privada

--- portfolio/backend/app.js
+++ portfolio/backend/app.js
@@ -43,6 +43,20 @@ async function traerData(){
   }
 }
 
+secured = async(req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();
+    } else{
+      res.redirect('/admin/login')
+    }
+  } catch (error){
+    console.log(error)
+  }
+
+}
+
 
 async function main(){
   //const nombre_completo = await traerData();
@@ -52,7 +66,10 @@ async function main(){
   const projects = await pool.query("SELECT * FROM proyectos").then((result) => {return result})
   console.log(edu[0])
   console.log(nombre_completo)
-  app.get('/', function(req, res){
+
+
+
+  app.get('/', secured,function(req, res){
     var conocido = Boolean(req.session.nombre);
     if (conocido){
       res.render('index', {
@@ -74,21 +91,18 @@ async function main(){
     }
   });
 
-  app.post('/ingresar', function (req, res){
-    req.session.nombre = req.body.nombre
-    res.redirect('/')
-  })
+  // app.post('/ingresar', function (req, res){
+  //   req.session.nombre = req.body.nombre
+  //   res.redirect('/')
+  // })
 
-  app.get('/logout', function (req,res){
-    req.session.destroy();
-    res.redirect('/');
-  });
+
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-// app.use('/admin/login', loginRouter);
+  app.use('/admin/login', loginRouter);
 
-  app.get('/education', function (req,res){
+  app.get('/education', secured ,function (req,res){
     res.render('education/education', {
       edu_uni: edu[0],
       edu_terciaria: edu[1],
@@ -96,7 +110,7 @@ async function main(){
     })
   });
 
-  app.get('/experience', function (req,res){
+  app.get('/experience', secured,function (req,res){
     res.render('experience/experience', {
       exp_inv: exp[0],
       exp_ava: exp[1],
@@ -104,7 +118,7 @@ async function main(){
     })
   })
 
-  app.get('/projects', function (req,res){
+  app.get('/projects', secured,function (req,res){
     res.render('projects/projects', {
       tic_tac: projects[0],
       portfolio: projects[1],
@@ -131,6 +145,8 @@ async function main(){
     res.status(err.status || 500);
     res.render('error');
   });
+
+
 
 
 
